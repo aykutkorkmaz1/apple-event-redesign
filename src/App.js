fa8c49db658ee474470791a998b77909bad7b285
Aykut Korkmaz <aykutkorkmaz.original@gmail.com>
2022-03-19 14:59:07 +0300

    main page new design

--- src/App.js
+++ src/App.js
@@ -16,77 +16,58 @@ function App() {
     <div className="App">
       <Navbar />
 
-    <div className='iphone13pro-content'>
-      <img src={iphone13pro_logo} />
-      <div className='iphone13pro_slogan'>Oh. So. Pro.</div>
-      <div className='description'>From $42.62/mo. for 24 mo. or $999 before trade-in</div>
-      <div className='blue-button'>
-        <span>Learn more</span>
+      <div className="main-iphone13pro">
+          <div className="align-items">
+            <img src={iphone13pro_logo} />
+            <span className="main-page-slogan">Oh. So. Pro.</span>
+            <span className="main-page-desc">From $42.62/mo for 24 mo. or $999 before trade-in.</span>
+             <div className='main-blue-button'>
+              <span>Learn more</span>
+            </div>
+          </div>
       </div>
-    </div>
 
-    <div className='iphonese-content'>
-      <div className='iphone-image'></div>
-      <div className='iphone_info'>
-        <img src={iphonese_logo} />
-        <div className='iphonese_slogan'>
-          <span>Superpowerful A15 Bionic chip.</span> <br />
-          <span>Superstar camera.</span> <br />
-          <span>Big-time battery.</span> <br />
-          <span>All at an incredible price.</span>
-        </div>
-        <div className='iphonese_buttons'>
-          <div className='bordered-button'>
-            <span>Learn more</span>
+      <div className="main-iphonese">
+          <div className="align-items">
+            <img src={iphonese_logo} />
+            <span className="main-page-slogan">Hercules in your pocket.</span>
+            <span className="main-page-desc">Just starting from $429.</span>
+             <div className='main-blue-button'>
+              <span>Learn more</span>
+            </div>
           </div>
-          <div className='bordered-button'>
-            <span>View pricing</span>
-          </div>
-        </div>
       </div>
-    </div>
 
-    <div className='macbookpro-content'>
-      <div className='macbookpro-main'>
-      <img src={macbookpro_logo} />
-      <div className='macbookpro_slogan'>Supercharged for pros.</div>
-      <div className='macbookpro_description'>
-        The most powerful MacBook Pro ever is here.
-        With the blazing-fast M1 Pro or M1 Max chip you get groundbreaking performance and amazing battery life.
-        Add to that a stunning Liquid Retina XDR display, the best camera and audio ever in a Mac notebook, and all the ports you need.
-        The first notebook of its kind, this MacBook Pro is a beast.
-      </div>
-      <div className='macbookpro_buttons'>
-          <div className='bordered-button'>
-            <span>Learn more</span>
+      <div className="main-macbookpro">
+          <div className="align-items">
+            <img src={macbookpro_logo} />
+            <span className="main-page-slogan">Supercharged for pros.</span>
+            <span className="main-page-desc">
+            The most powerful MacBook Pro ever is here.
+            With the blazing-fast M1 Pro or M1 Max chip you get groundbreaking performance and amazing battery life.
+            Add to that a stunning Liquid Retina XDR display, the best camera and audio ever in a Mac notebook,
+            and all the ports you need. The first notebook of its kind, this MacBook Pro is a beast.
+            </span>
+             <div className='main-blue-button'>
+              <span>Learn more</span>
+            </div>
           </div>
-          <div className='bordered-button'>
-            <span>View pricing</span>
-          </div>
-        </div>
       </div>
-    </div>
 
-    <div className='ipad-content'>
-      <div className='ipad-main'>
-      <img src={ipad_logo} />
-      <div className='ipad_slogan'>
-          <span>Light. Bright.</span> <br />
-          <span>Full of might.</span> <br />
-          <span>Now supercharged by Apple M1.</span>
-      </div>
-      <div className='ipad_buttons'>
-          <div className='bordered-button'>
-            <span>Learn more</span>
+      <div className="main-ipad">
+          <div className="align-items">
+            <img src={ipad_logo} />
+            <span className="main-page-slogan">Light. Bright. Full of might.</span>
+            <span className="main-page-desc">
+            Now powered by Apple M1 chip.
+            </span>
+             <div className='main-blue-button'>
+              <span>Learn more</span>
+            </div>
           </div>
-          <div className='bordered-button'>
-            <span>View pricing</span>
-          </div>
-        </div>
       </div>
-    </div>
 
-    <Footer />
+      
 
     </div>
   );
